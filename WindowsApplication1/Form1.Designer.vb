7ccca02562630341870e6f44264f57a941432e68
Billy <billy@multiseis.com>
2015-11-09 18:07:56 +0000

    Initial commit.

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.DirectoryEntry1 = New System.DirectoryServices.DirectoryEntry()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.red
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(207, 31)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(402, 31)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(12, 212)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(207, 212)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(402, 212)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox7.Image = CType(resources.GetObject("PictureBox7.Image"), System.Drawing.Image)
+        Me.PictureBox7.Location = New System.Drawing.Point(12, 393)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 6
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox8.Image = CType(resources.GetObject("PictureBox8.Image"), System.Drawing.Image)
+        Me.PictureBox8.Location = New System.Drawing.Point(207, 393)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 7
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox9.Image = CType(resources.GetObject("PictureBox9.Image"), System.Drawing.Image)
+        Me.PictureBox9.Location = New System.Drawing.Point(402, 393)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(189, 175)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 8
+        Me.PictureBox9.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(917, 28)
+        Me.MenuStrip1.TabIndex = 9
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(114, 26)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(766, 505)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(139, 21)
+        Me.CheckBox1.TabIndex = 10
+        Me.CheckBox1.Text = "Play against CPU"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 611)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(917, 22)
+        Me.StatusStrip1.TabIndex = 11
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(597, 31)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(308, 468)
+        Me.ListBox1.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(917, 633)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Tic Tac Toe - Adriana"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox9 As System.Windows.Forms.PictureBox
+    Friend WithEvents DirectoryEntry1 As System.DirectoryServices.DirectoryEntry
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,543 @@
+﻿
+Public Class Form1
+
+    Dim PlayMatrix(2, 2) As Char
+    Dim PlayImage(2, 2) As PictureBox
+    Dim LastPlay As Char
+    Dim StopPlaying As Boolean
+    Dim PlayAgainstCPU As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ReloadGame()
+
+    End Sub
+
+    Private Sub ReloadGame()
+
+        PlayImage(0, 0) = PictureBox1
+        PlayImage(0, 1) = PictureBox2
+        PlayImage(0, 2) = PictureBox3
+        PlayImage(1, 0) = PictureBox4
+        PlayImage(1, 1) = PictureBox5
+        PlayImage(1, 2) = PictureBox6
+        PlayImage(2, 0) = PictureBox7
+        PlayImage(2, 1) = PictureBox8
+        PlayImage(2, 2) = PictureBox9
+
+        LastPlay = "r"
+
+        For i = 0 To 2
+            For j = 0 To 2
+                PlayMatrix(i, j) = "e"
+                PlayImage(i, j).Image = Nothing
+            Next
+        Next
+
+        StopPlaying = False
+        PlayAgainstCPU = CheckBox1.Checked
+
+    End Sub
+
+    Private Sub DrawGame()
+
+        ListBox1.Items.Insert(0, "Draw game.")
+        My.Computer.Audio.Play(My.Resources.boring, AudioPlayMode.Background)
+        StopPlaying = True
+
+    End Sub
+
+    Private Sub PlayerWins(Player As Char)
+
+        If (Player = "b") Then
+            ListBox1.Items.Insert(0, "Player blue wins.")
+            If (PlayAgainstCPU) Then
+                My.Computer.Audio.Play(My.Resources.woohoo, AudioPlayMode.Background)
+            Else
+                My.Computer.Audio.Play(My.Resources.woohoo, AudioPlayMode.Background)
+            End If
+            StopPlaying = True
+        ElseIf (Player = "r") Then
+            ListBox1.Items.Insert(0, "Player red wins.")
+            If (PlayAgainstCPU) Then
+                My.Computer.Audio.Play(My.Resources.doh, AudioPlayMode.Background)
+            Else
+                My.Computer.Audio.Play(My.Resources.woohoo, AudioPlayMode.Background)
+            End If
+            StopPlaying = True
+        End If
+
+    End Sub
+
+    Private Sub CheckBoard()
+
+        For i = 0 To 2
+            If (PlayMatrix(i, 0) = "r") And (PlayMatrix(i, 1) = "r") And (PlayMatrix(i, 2) = "r") Then
+                PlayerWins("r")
+                Return
+            End If
+            If (PlayMatrix(i, 0) = "b") And (PlayMatrix(i, 1) = "b") And (PlayMatrix(i, 2) = "b") Then
+                PlayerWins("b")
+                Return
+            End If
+        Next
+
+        For j = 0 To 2
+            If (PlayMatrix(0, j) = "r") And (PlayMatrix(1, j) = "r") And (PlayMatrix(2, j) = "r") Then
+                PlayerWins("r")
+                Return
+            End If
+            If (PlayMatrix(0, j) = "b") And (PlayMatrix(1, j) = "b") And (PlayMatrix(2, j) = "b") Then
+                PlayerWins("b")
+                Return
+            End If
+        Next
+
+        If (PlayMatrix(0, 0) = "r") And (PlayMatrix(1, 1) = "r") And (PlayMatrix(2, 2) = "r") Then
+            PlayerWins("r")
+            Return
+        End If
+
+        If (PlayMatrix(0, 2) = "r") And (PlayMatrix(1, 1) = "r") And (PlayMatrix(2, 0) = "r") Then
+            PlayerWins("r")
+            Return
+        End If
+
+        If (PlayMatrix(0, 0) = "b") And (PlayMatrix(1, 1) = "b") And (PlayMatrix(2, 2) = "b") Then
+            PlayerWins("b")
+            Return
+        End If
+
+        If (PlayMatrix(0, 2) = "b") And (PlayMatrix(1, 1) = "b") And (PlayMatrix(2, 0) = "b") Then
+            PlayerWins("b")
+            Return
+        End If
+
+        If (Not StopPlaying)
+
+            Dim c As Integer
+
+            c = 0
+
+            For i = 0 To 2
+                For j = 0 To 2
+                    If PlayMatrix(i, j) <> "e" Then
+                        c = c + 1
+                    End If
+                Next
+            Next
+
+            If (c = 9) Then
+                DrawGame()
+                Return
+            End If
+        End If
+
+    End Sub
+
+    Private Function CheckPlayerWin(Player As Char, ByRef ii As Integer, ByRef jj As Integer) As Boolean
+
+        Dim p, e As Integer
+        Dim j As Integer
+
+        ' Check rows
+        For i = 0 To 2
+
+            p = 0
+            e = -1
+
+            For j = 0 To 2
+
+                If (PlayMatrix(i, j) = Player) Then
+
+                    p = p + 1
+
+                ElseIf (PlayMatrix(i, j) = "e") Then
+
+                    e = j
+
+                    ii = i
+                    jj = e
+
+                End If
+            Next
+
+            If (p = 2 And e <> -1) Then
+                Return True
+            End If
+
+        Next
+
+        ' Check cols
+        For j = 0 To 2
+
+            p = 0
+            e = -1
+
+            For i = 0 To 2
+
+                If (PlayMatrix(i, j) = Player) Then
+
+                    p = p + 1
+
+                ElseIf (PlayMatrix(i, j) = "e") Then
+
+                    e = i
+
+                    ii = e
+                    jj = j
+
+                End If
+            Next
+
+            If (p = 2 And e <> -1) Then
+                Return True
+            End If
+
+        Next
+
+        ' Check diagonals
+        ' Check 1 diagonal
+        p = 0
+        e = -1
+
+        For i = 0 To 2
+
+            j = i
+
+            If (PlayMatrix(i, j) = Player) Then
+
+                p = p + 1
+
+            ElseIf (PlayMatrix(i, j) = "e") Then
+
+                e = j
+
+                ii = i
+                jj = e
+
+            End If
+
+        Next
+
+        If (p = 2 And e <> -1) Then
+            Return True
+        End If
+
+        ' Check 2 diagonal
+        p = 0
+        e = -1
+
+        For i = 0 To 2
+
+            j = 2 - i
+
+            If (PlayMatrix(i, j) = Player) Then
+
+                p = p + 1
+
+            ElseIf (PlayMatrix(i, j) = "e") Then
+
+                e = j
+
+                ii = i
+                jj = e
+
+            End If
+
+        Next
+
+        If (p = 2 And e <> -1) Then
+            Return True
+        End If
+
+        Return False
+
+    End Function
+
+    Private Sub ComputerPlay()
+
+        Dim i, j As Integer
+
+        'First try to win
+        If (CheckPlayerWin("r", i, j)) Then
+            PlayPosition(i, j)
+            Return
+        End If
+
+        'Second stop opponent
+        If (CheckPlayerWin("b", i, j)) Then
+            PlayPosition(i, j)
+            Return
+        End If
+
+        'Third play middle
+        If (PlayMatrix(1, 1) = "e") Then
+
+            PlayPosition(1, 1)
+            Return
+
+        End If
+
+        'Fourth play corners
+        If (PlayMatrix(0, 0) = "e") Then
+
+            PlayPosition(0, 0)
+            Return
+
+        End If
+
+        If (PlayMatrix(0, 2) = "e") Then
+
+            PlayPosition(0, 2)
+            Return
+
+        End If
+
+        If (PlayMatrix(2, 2) = "e") Then
+
+            PlayPosition(2, 2)
+            Return
+
+        End If
+
+        If (PlayMatrix(2, 0) = "e") Then
+
+            PlayPosition(2, 0)
+            Return
+
+        End If
+
+        ' Fifth play middle edges
+        If (PlayMatrix(0, 1) = "e") Then
+
+            PlayPosition(0, 1)
+            Return
+
+        End If
+
+        If (PlayMatrix(1, 2) = "e") Then
+
+            PlayPosition(1, 2)
+            Return
+
+        End If
+
+        If (PlayMatrix(2, 1) = "e") Then
+
+            PlayPosition(2, 1)
+            Return
+
+        End If
+
+        If (PlayMatrix(1, 0) = "e") Then
+
+            PlayPosition(1, 0)
+            Return
+
+        End If
+
+    End Sub
+
+    Private Sub PlayPosition(i As Integer, j As Integer)
+
+        If (PlayMatrix(i, j) = "e") And Not StopPlaying Then
+
+            If (LastPlay = "r") Then
+                PlayMatrix(i, j) = "b"
+                PlayImage(i, j).Image = My.Resources.blue
+                LastPlay = "b"
+            ElseIf (LastPlay = "b") Then
+                PlayMatrix(i, j) = "r"
+                PlayImage(i, j).Image = My.Resources.red
+                LastPlay = "r"
+            End If
+
+            CheckBoard()
+
+            If (PlayAgainstCPU And LastPlay = "b") Then
+
+                ComputerPlay()
+
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        PlayPosition(0, 0)
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+        PlayPosition(0, 1)
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+        PlayPosition(0, 2)
+
+    End Sub
+
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+
+        PlayPosition(1, 0)
+
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+
+        PlayPosition(1, 1)
+
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+
+        PlayPosition(1, 2)
+
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+
+        PlayPosition(2, 0)
+
+    End Sub
+
+    Private Sub PictureBox8_Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+
+        PlayPosition(2, 1)
+
+    End Sub
+
+    Private Sub PictureBox9_Click(sender As Object, e As EventArgs) Handles PictureBox9.Click
+
+        PlayPosition(2, 2)
+
+    End Sub
+
+    Private Sub PictureBox1_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox1.MouseEnter
+
+        PlayImage(0, 0).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox1_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox1.MouseLeave
+
+        PlayImage(0, 0).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub PictureBox2_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox2.MouseEnter
+
+        PlayImage(0, 1).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox2_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox2.MouseLeave
+
+        PlayImage(0, 1).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub PictureBox3_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox3.MouseEnter
+
+        PlayImage(0, 2).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox3_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox3.MouseLeave
+
+        PlayImage(0, 2).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub PictureBox4_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox4.MouseEnter
+
+        PlayImage(1, 0).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox4_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox4.MouseLeave
+
+        PlayImage(1, 0).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub PictureBox5_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox5.MouseEnter
+
+        PlayImage(1, 1).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox5_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox5.MouseLeave
+
+        PlayImage(1, 1).BackColor = Control.DefaultBackColor
+        
+    End Sub
+
+    Private Sub PictureBox6_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox6.MouseEnter
+
+        PlayImage(1, 2).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox6_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox6.MouseLeave
+
+        PlayImage(1, 2).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub PictureBox7_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox7.MouseEnter
+
+        PlayImage(2, 0).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox7_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox7.MouseLeave
+
+        PlayImage(2, 0).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub PictureBox8_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox8.MouseEnter
+
+        PlayImage(2, 1).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox8_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox8.MouseLeave
+
+        PlayImage(2, 1).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub PictureBox9_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox9.MouseEnter
+
+        PlayImage(2, 2).BackColor = Color.LightGray
+
+    End Sub
+
+    Private Sub PictureBox9_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox9.MouseLeave
+
+        PlayImage(2, 2).BackColor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+
+        ReloadGame()
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        PlayAgainstCPU = CheckBox1.Checked
+
+    End Sub
+
+End Class
